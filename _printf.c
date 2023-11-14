2a788a93a12a0fd7ec9d248645d0ce0a1ef1d1ec
kevinbrianndungu <kevinbrianndungu>
2023-11-14 13:40:43 -0800

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,22 +1,20 @@
 #include "main.h"
 
 /**
- * _printf - our printf function
- * @format: The string
- * @...: argument variable
+ * _printf - Custom printf function
+ * @format: The format string
+ * @...: Variable arguments
  *
- * Return: count the number of characters printed
+ * Return: The number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	int char_count = 0;
-	va_list args;
+    int char_count = 0; /* Initialize the character count */
+    va_list args; /* Declare a variable list to hold the variable arguments */
 
-	va_start(args, format);
-	_format(format, args, &char_count);
-	va_end(args);
+    va_start(args, format); /* Start processing the variable arguments */
+    _format(format, args, &char_count); /* Call the _format function to process the format string */
+    va_end(args); /* End processing the variable arguments */
 
-	return (char_count);
+    return char_count; /* Return the total number of characters printed */
 }
-
-
